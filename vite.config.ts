d45b1dd32f3b5586d6d4648b1045cecffbb96882
dreambo8563 <dreambo8563@outlook.com>
2025-12-05 19:22:58 +0800

    feat: Update build configuration and .gitignore settings
    - Changed the output directory in vite.config.ts to 'docs' for build artifacts.
    - Updated .gitignore to include the 'dist' directory for proper exclusion.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      build: {
+        outDir: 'docs',
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
